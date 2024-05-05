4e3743fe89c4a4c8485ee006287577031a0ea8c5
yasmeenTarekKhater <yasmeentarek112001@gmail.com>
2024-05-05 19:21:06 +0300

    handle some issues
    add active calass to navbar links
    handle background image for header
    handle responsive for nav and header

--- js/home.js
+++ js/home.js
@@ -1,13 +1,19 @@
-//----------changing features image on hover links----------------------------------------------------
-const links = document.querySelectorAll('.features-dropdown a');
+//----------add active class to navbar links && changing features image on hover links--------------
+const navBarFeaturelinks = document.querySelectorAll('.features-dropdown a');
+const navBarlinks = document.querySelectorAll('.navbar-pages');
 const featuresImages=document.querySelectorAll('.features-dropdown-images');
 
-// Add event listeners to the links
-links.forEach((link, index) => {
+navBarlinks.forEach(navLink=>{
+      navLink.addEventListener('click',function(){
+        navBarlinks.forEach(navLink=>{navLink.classList.remove('active')}); 
+        this.classList.add('active');
+      });
+});
+
+navBarFeaturelinks.forEach((link, index) => {
     link.addEventListener('mouseenter', () => {
       // Hide all images
       featuresImages.forEach(image => image.classList.add('hide-features-images'));
-  
       // Show the corresponding image
       featuresImages[index+1].classList.remove('hide-features-images');
     });
@@ -15,13 +21,12 @@ links.forEach((link, index) => {
     link.addEventListener('mouseleave', () => {
       // Show the default image
       featuresImages[0].classList.remove('hide-features-images');
-  
       // Hide other images
       for (let i = 1; i < featuresImages.length; i++) {
         featuresImages[i].classList.add('hide-features-images');
       }
     });
-  });
+});
 //----------End changing features image on hover links---------------------------------------------
 
 //----------rigth side magical circle section animation---------------------------------------------
