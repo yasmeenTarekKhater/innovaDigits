637e1417d94d6326a35abba2c82c387daf854ab0
yasmeenTarekKhater <yasmeentarek112001@gmail.com>
2024-05-07 18:30:18 +0300

    finsh all responsive
    also seperate shared logic of js to another file still i need fix issues in slider js code

--- js/home.js
+++ js/home.js
@@ -1,34 +1,3 @@
-//----------add active class to navbar links && changing features image on hover links--------------
-const navBarFeaturelinks = document.querySelectorAll('.features-dropdown a');
-const navBarlinks = document.querySelectorAll('.navbar-pages');
-const featuresImages=document.querySelectorAll('.features-dropdown-images');
-
-navBarlinks.forEach(navLink=>{
-      navLink.addEventListener('click',function(){
-        navBarlinks.forEach(navLink=>{navLink.classList.remove('active')}); 
-        this.classList.add('active');
-      });
-});
-
-navBarFeaturelinks.forEach((link, index) => {
-    link.addEventListener('mouseenter', () => {
-      // Hide all images
-      featuresImages.forEach(image => image.classList.add('hide-features-images'));
-      // Show the corresponding image
-      featuresImages[index+1].classList.remove('hide-features-images');
-    });
-  
-    link.addEventListener('mouseleave', () => {
-      // Show the default image
-      featuresImages[0].classList.remove('hide-features-images');
-      // Hide other images
-      for (let i = 1; i < featuresImages.length; i++) {
-        featuresImages[i].classList.add('hide-features-images');
-      }
-    });
-});
-//----------End changing features image on hover links---------------------------------------------
-
 //----------rigth side magical circle section animation---------------------------------------------
 
 const magicCircleContainer=document.querySelector(".magical-circle-rigth");
@@ -196,17 +165,3 @@ questionSliderArrows.forEach((arrow)=>{
     });
 });
 //--------------------End  Question and Languages slider-------------------
-
-const freeTrailContainer=document.querySelector(".free-trail-container");
-const freeTrailContent=document.querySelectorAll(".free-trail-content div");
-const freeTrailShadow=document.querySelector(".free-trail-shadow");
-const freeTrailCkasses=["free-trail-container-left-move","free-trail-container-rigth-move"];
-
-
-freeTrailContainer.addEventListener('click',function(){
-  freeTrailContent.forEach((content,index)=>{
-    content.classList.toggle(freeTrailCkasses[index]);
-  });
-  freeTrailShadow.classList.toggle("free-trail-shadow-transparent");
-  this.classList.toggle("free-trail-container-small");
-});
